43b5e30f5acf0956b252a67a40cfb2bbe59a46a2
k8517 <k8517@student.jamk.fi>
2017-01-11 15:16:53 +0200

    BlackJack-update. Added Lotto.

--- BlackJackV0/BlackJackV0/BlackJack.cs
+++ BlackJackV0/BlackJackV0/BlackJack.cs
@@ -1,34 +1,107 @@
-﻿using System;
-/// <summary>
-/// A simple example of Blackjack card game
-/// </summary>
+﻿/* **************************************
+BlackJack-harjoitus
+
+Luotu 11.1.2017
+
+Toiminta: Käyttäjältä kysytään numeroa väliltä 1-21. Annettua numeroa verrataan
+koneen arpomaan lukuun (arpoo väliltä 10-21), mikäli käyttäjän luku on suurempi, 
+käyttäjä voittaa. Pienemmällä luvulla käyttäjä häviää.
+
+Mikäli syötetään 'x' tai 'exit', ohjelma lopetetaan.
+
+Minttu Mäkäläinen K8517 @ JAMK
+Viety GitHubiin 11.1.2017
+************************************** */
+
+using System;
+using System.Linq;
+
 namespace JAMK.IT
 {
   class BlackJack
   {
     static void Main()
-    {
-      int myNumber = 17;
-      int theirNumber;
-      System.Console.WriteLine("*** BlackJack! ***");
-      System.Console.Write("Can you beat my number? Enter any number between 1-21: ");
-      //reading and converting 
-      theirNumber = System.Convert.ToInt32(System.Console.ReadLine());
-      //comparing that given umber is valid
-      if (theirNumber < 1 || theirNumber > 21)
-      {
-        Console.WriteLine("The given number is out of limits, try again.");
-      }
-      else {
-        //comparing
-        if (theirNumber >= myNumber && theirNumber <= 21)
         {
-          System.Console.WriteLine("You win.");
+            // Lottokone.DrawLotto();           // Lottokone
+            Uhkapeli();                         // BlackJack
         }
-        else {
-          System.Console.WriteLine("You lose.");
+
+        static void Uhkapeli()
+            {
+                int myNumber;
+                string muuttuja;
+                int theirNumber;
+
+                Random rand = new Random();
+
+                System.Console.WriteLine("*** BlackJack! ***");
+
+                do
+                {
+                    System.Console.Write("Can you beat my number? Enter any number between 1-21: ");
+
+                    muuttuja = System.Console.ReadLine();                       // reading input
+
+                    if (muuttuja == "x" || muuttuja == "exit")
+                    {
+                        break;
+                    }
+
+                try
+                {
+                    theirNumber = Convert.ToInt32(muuttuja);
+
+                    if (theirNumber < 1 || theirNumber > 21)            // comparing that given number is valid
+                    {
+                        Console.WriteLine("The given number is out of limits, try again. (1-21)");
+                    }
+
+                    else
+                    {
+                        myNumber = rand.Next(10, 21);                       // random number between 10-21
+                        Console.WriteLine("My number is " + myNumber + ", and your number is " + theirNumber + ".");
+
+                        if (theirNumber >= myNumber && theirNumber <= 21)   //comparing
+                        {
+                            System.Console.WriteLine("You win.");
+                        }
+                        else
+                        {
+                            System.Console.WriteLine("You lose.");
+                        }
+                    }
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Please enter any number between 1-21. Enter 'x' or 'exit' to quit.");
+                }
+
+                
+
+                /*if (muuttuja.GetType() == typeof(int))
+                    {
+                        theirNumber = Convert.ToInt32(muuttuja);            // converting to int
+
+                        if (theirNumber < 1 || theirNumber > 21)            // comparing that given number is valid
+                        {
+                            Console.WriteLine("The given number is out of limits, try again.");
+                        }
+
+                        myNumber = rand.Next(10, 21);                       // random number between 10-21
+                        Console.WriteLine("My number is " + myNumber + ", and your number is " + theirNumber + ".");
+
+                        if (theirNumber >= myNumber && theirNumber <= 21)   //comparing
+                        {
+                            System.Console.WriteLine("You win.");
+                        }
+                        else
+                        {
+                            System.Console.WriteLine("You lose.");
+                        }
+                    }*/
+
+            } while (true);
+            
         }
-      }
-    }
   }
 }
---
+++ BlackJackV0/BlackJackV0/Program.cs
@@ -0,0 +1,65 @@
+﻿/* **************************************
+Lottoharjoitus
+
+Luotu 10.1.2017
+
+Toiminta: Lottosovellus, jossa arvotaan 7 satunnaista numeroa 40 numerosta (1-40).
+Käyttäjältä kysytään, kuinka monta lottoriviä halutaan arpoa.
+Samalle riville ei saa tulla jo arvottuja numeroita.
+
+Minttu Mäkäläinen K8517 @ JAMK
+Viety GitHubiin 11.1.2017
+************************************** */
+
+using System;
+using System.Linq;
+
+namespace JAMK.IT                           // Määrittelee tämän sovelluksen nimiavaruuden
+{
+    public class Lottokone                  // Määrittelee sovelluksen pääluokan
+    {
+        public static void DrawLotto()
+        {
+            int numero = 0, i = 0, e = 1;   // Esitellään ja alustetaan muuttujia
+            int rivit;
+
+            int[] lotto = new int[7];       // Lottonumeroille taulukko = 7 paikkaa
+
+            Random rand = new Random();
+
+            Console.Write("Kuinka monta lottoriviä arvotaan? ");
+            rivit = int.Parse(Console.ReadLine());  // Otetaan annettu lottorivimäärä talteen
+
+            while (e <= rivit)                      // Tehdään niin monta riviä, kuin on haluttu
+            {
+                while (i < lotto.Length)            // Pyöritetään niin kauan kuin taulukkoon vielä mahtuu uusia numeroita
+                {
+                    numero = rand.Next(1, 40);      // Arvotaan satunnaislukugeneraattorin avulla numero väliltä 1-40
+
+                    if (!lotto.Contains(numero))    // Jos arvottu numero ei vielä löydy taulukosta
+                    {
+                        lotto[i] = numero;          // Tallennetaan arvottu numero taulukkoon
+                        i++;
+                    }
+                }
+
+                Array.Sort(lotto);                  // Lajittelee numerot suuruusjärjestykseen
+
+                Console.Write("\nRivi " + e + ": ");    // \n = rivinvaihto
+
+                for (int r = 0; r < lotto.Length; r++)  // Tulostaa arvotun rivin
+                {
+                    Console.Write(lotto[r] + ", ");
+                }
+
+                lotto = new int[7];                 // Alustetaan taulukko uudelleen
+
+                e++;
+                i = 0;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
